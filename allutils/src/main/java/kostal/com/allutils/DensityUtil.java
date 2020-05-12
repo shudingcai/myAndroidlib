1c2a5ebddff7ba3a87d3ef8e62da16fa94eef325
shudingcai <shudingcai@163.com>
2020-05-12 13:10:46 +0800

    Initial commit

---
+++ allutils/src/main/java/kostal/com/allutils/DensityUtil.java
@@ -0,0 +1,24 @@
+package kostal.com.allutils;
+
+import android.content.Context;
+
+/**
+ * 作用：像素和 dp 转换类；代码中设置尺寸单位是 像素；对于不同分辨率的手机，效果差别大；
+ */
+public class DensityUtil {
+    /**
+     * 根据手机的分辨率从 dip 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
